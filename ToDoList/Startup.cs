a6b8964937ee9a11e98f57f034f0053a86c4236d
Domenico Emanuele Giannone <domgia@xdatanet.com>
2024-06-04 13:02:52 +0200

    Creazione del servizio di autenticazione

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -7,6 +7,12 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.Owin.Security.Jwt;
 using Microsoft.Owin.Security;
 using System.Text;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+using System.Threading.Tasks;
+
+
+
 
 [assembly: OwinStartup(typeof(ToDoList.Startup))]
 
@@ -16,67 +22,21 @@ namespace ToDoList
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
-        }
-        public static void ConfigureAuth(IAppBuilder app)
-        {
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            var myProvider = new MyAuthorizationServerProvider();
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
             {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Home/Login"),
-                Provider = new CookieAuthenticationProvider
-                {
-                    // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
-                    //OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                    //    validateInterval: TimeSpan.FromMinutes(30),
-                    //    regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                }
-            });
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
-
-            // Enables the application to remember the second login verification factor such as phone or email.
-            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-            // This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = myProvider
+            };
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
 
 
-            //setup JWT
-            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
-            {
-                AuthenticationMode = AuthenticationMode.Active,
-                TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidAudience = GetAudience(),
-                    ValidIssuer = GetIssuer(),
-                    IssuerSigningKey = GetSymmetricSecurityKey(),
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true
-                }
-            });
-        }
-        public static string GetIssuer()
-        {
-            return "My Issuer";
-        }
-        public static string GetAudience()
-        {
-            return "My Audience";
-        }
-        public static string GetSecurityKey()
-        {
-            return "My Super Secrect Security key that is at least 32 characters";
-        }
-        public static SymmetricSecurityKey GetSymmetricSecurityKey()
-        {
-            var issuerSigningKey = GetSecurityKey();
-            byte[] data = Encoding.UTF8.GetBytes(issuerSigningKey);
-
-            var result = new SymmetricSecurityKey(data);
-            return result;
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
         }
     }
 }
